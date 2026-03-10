202e2cb58dff2c8e6d585a34b83fa1af0368c27b
Dhiego <dhiegodev@hotmail.com>
2026-03-09 22:28:51 -0300

    POST de categoria

--- src/main/java/br/com/fiap/ecommerce/api/categoria/DadosCadastroCategoria.java
+++ src/main/java/br/com/fiap/ecommerce/api/categoria/DadosCadastroCategoria.java
@@ -1,5 +1,17 @@
 package br.com.fiap.ecommerce.api.categoria;
 
-public record DadosCadastroCategoria(String nome, String descricao) {
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record DadosCadastroCategoria(
+        @NotBlank // Garante que não seja nulo e nem em branco
+        @Size(min = 3, max = 60)
+        @Column(unique = true)
+        String nome,
+
+
+        @Size(max = 255)
+        String descricao) {
 
 }
--- src/main/java/br/com/fiap/ecommerce/api/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/ecommerce/api/controller/CategoriaController.java
@@ -1,18 +1,34 @@
 package br.com.fiap.ecommerce.api.controller;
 
 
+import br.com.fiap.ecommerce.api.categoria.Categoria;
+import br.com.fiap.ecommerce.api.categoria.CategoriaRepository;
 import br.com.fiap.ecommerce.api.categoria.DadosCadastroCategoria;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.ecommerce.api.categoria.DadosListagemCategoria;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("categorias")
+import java.util.List;
+
+@RestController // Define que esta classe é um controller REST
+@RequestMapping("categorias") // define o caminho que receberá as requisições
 public class CategoriaController {
 
-    @PostMapping
-    public void cadastrarCategoria(@RequestBody DadosCadastroCategoria dados){
-        System.out.println(dados.nome());
+    @Autowired // Spring instancia o objeto para nós
+    private CategoriaRepository categoriaRepository;
+
+    @Transactional // Rollback no banco em caso de erros
+    @PostMapping ///  recebe as requisições do tipo POST
+
+    //Recebe o conteúdo de Body e garante que é válido conforme as restrições do DTO
+    public void cadastrarCategoria(@RequestBody @Valid DadosCadastroCategoria dados){
+        categoriaRepository.save(new Categoria(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemCategoria> listarCategorias() {
+        return categoriaRepository.findAll().stream().map(DadosListagemCategoria::new).toList();
     }
 }
